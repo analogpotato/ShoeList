763be607dabb0af74330808379d8b949060cc6c1
Frank Foster <analogpotato@gmail.comn>
2018-12-15 13:15:52 -0800

    Initial Commit

---
+++ ShoeList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoeList
+//
+//  Created by Frank on 12/15/18.
+//  Copyright © 2018 Analog Potato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
